8457699778b7c90d1bc2f9f49e3e3645fac946e9
William Pepple <36817380+williampepple1@users.noreply.github.com>
2022-04-08 08:59:04 +0100

    Routes Added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,6 +9,8 @@ const protect = asyncHandler(async(req, res, next) => {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             req.user = await User.findById(decoded.id).select('-password')
+
+            next()
         }catch(error){
             console.log(error)
             res.status(401)
