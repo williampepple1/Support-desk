d583c63afbd4d156f3566f9aa351ae24bf6e2a66
William Pepple <36817380+williampepple1@users.noreply.github.com>
2022-04-08 12:04:29 +0100

    User registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,10 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import {FaUser} from 'react-icons/fa'
 import {toast} from 'react-toastify'
 import {useSelector, useDispatch} from 'react-redux'
-import {register} from '../features/auth/authSlice'
+import {register, reset } from '../features/auth/authSlice'
+
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,8 +17,23 @@ function Register() {
   const {name, email, password, password2} = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const {user, isLoading, isSuccess, message} = useSelector((state) => state.auth)
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message)
+    }
+    
+    if (isSuccess || user){
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, user, isSuccess, message, navigate, dispatch]);
+
+
 
   const onChange = (e) => {
     setFormData((prevState) => ({
